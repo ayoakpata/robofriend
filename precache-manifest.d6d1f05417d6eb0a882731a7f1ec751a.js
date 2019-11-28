6e90a0082c8a584ed6f2599feee03eb35fc7d30e
ayoakpata <akpata.ayobami@gmail.com>
2019-11-28 07:38:15 +0100

    Updates

---
+++ precache-manifest.d6d1f05417d6eb0a882731a7f1ec751a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce487bcfa36679e81d37127e7a0bfa15",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "6444c2fe514dbce15ab8",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3ce76db70e8984d86b90",
+    "url": "/robofriend/static/css/main.b4427f3d.chunk.css"
+  },
+  {
+    "revision": "6444c2fe514dbce15ab8",
+    "url": "/robofriend/static/js/2.48154e63.chunk.js"
+  },
+  {
+    "revision": "3ce76db70e8984d86b90",
+    "url": "/robofriend/static/js/main.e96c1159.chunk.js"
+  },
+  {
+    "revision": "8969822fc4f4a6cc6003",
+    "url": "/robofriend/static/js/runtime-main.8a3385a3.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriend/static/media/SEGA.82e09f78.TTF"
+  }
+]);
